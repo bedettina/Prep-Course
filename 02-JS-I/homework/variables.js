3f11d7a48a3b36965fd5564c6989472f4fd3a505
bedettina <bforbedetti@gmail.com>
2022-05-28 13:56:10 -0300

    Agrega definiciones

---
+++ 02-JS-I/homework/variables.js
@@ -0,0 +1,11 @@
+Variables: Las variables son nombres que se le dan a las ubicaciones que permiten guardar cosas, para que podamosutilizar más tarde. En JavaScript, se usa una forma de comunicación no tipada, lo que significa que esas
+"ubicaciones" a las que refieren las variables pueden cambiar su valor (en el ejemplo de las "ubicaciones", sería como si fueran nómades es decir, que pueden cambiar de ubicación o dirección).
+Son espacios de memoria donde se guarda información a la que luego se puede acceder.
+
+Strings: Son palabras o textos que van entre comillas.
+
+Funciones: Son objetos que se pueden llamar o invocar (como haciendo un Alohamora) y que a su vez tienen el poder de transformar a las variables. También sirven para calcularlas. 
+
+Declaraciones if: Son condiciones que le ponemos a la consola para devolvernos algo que le estamos pidiendo. Un ejemplo podría ser "Si sale menos de 2 pesos, dame un caramelo" (?)
+
+Valores booleanos (true,false): Es una forma de verificar si algo es cierto o falso.
