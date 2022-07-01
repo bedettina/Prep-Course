4143c5c904311ed6439b29e77cefa89de723ff16
bedettina <bforbedetti@gmail.com>
2022-07-01 16:13:29 -0300

    HTML y CSS homework

---
+++ 08-HTML/homework/homework.html.js
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title> Tarea HTML de Lucía</title>
+    </head>
+    <body>
+    <div> 
+        <h1> Lucía  </h1>
+        <h3> Henry  </h3>
+        <h4> tarea de HTML / CSS  </h4>
+    </div>
+
+    <div> 
+    <span style="color:purple">Mi comida favorita es el sushi</span>
+    <a id="Restaurante favorito" href="https://tangie.com.ar/">Tangie sushi</a>
+    </div>
+
+    <div>
+        <ul>
+            <li>Sushi</li>
+            <img id="Sushi" src="https://i0.wp.com/naturallynidhi.com/wp-content/uploads/2021/02/Spicy-Tofu-Veggie-Sushi-Rolls-Vegan-Cover.jpg?w=1200&ssl=1"
+            width="304" height="228"></img>
+            <li>Hamburguesas</li>
+            <img id="Hamburguesa" src="https://i.pinimg.com/originals/05/ce/52/05ce52a2f72a5fa35e50646a81fcb69b.jpg"
+             width="304" height="228"></img>
+        </ul>
+    </div>
+    </body>
+</html>
+
+
+
+/* II Añade un poco de estilo
+  A. Agregue la etiqueta de estilo a su documento HTML.
+  B. Asignar al tercer div el id "thirdDiv".
+  C. Asignar a los otros divs la clase "divClass".
+  D. Asignar al span que se encuentra dentro de su segundo div el id "spanId".
+  E. Agregue una regla de estilo a su elemento h1 cambiando el color del texto (Listado de colores: https://www.crockford.com/wrrrld/color.html).
+  F. Agregue una regla de estilo al elemento img dándole un ancho de 400px.
+  G. Agregue una regla de estilo a 'thirdDiv' cambiando la altura a 600px y el ancho a 500px.
+  H. Agregue una regla de estilo a 'thirdDiv' cambiando el color de fondo por el que quiera.
+  I. Cambie el tamaño de la fuente para 'spanId' a 18px.
+  J. Asignar al id 'spanId' un margen de 50px.
+  K. Asignar al id 'thirdDiv' un relleno (padding) de 50px.
+  L. Asignar al id 'thirdDiv' un borde con cualquier ancho de color y estilo que elija.*/
